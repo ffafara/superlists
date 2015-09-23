307a073f1d4879b119c09588a63f824fc1e70564
Filip Fafara <ffafara@thoughtworks.com>
2015-09-22 20:02:20 -0400

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -87,15 +87,3 @@ class NewVisitorTest(LiveServerTestCase):
 		# Satisfied, they both go back to sleep
 
 
-		self.fail('Finish the test!')
-
-		# The page updates again, and now shows both items on her list
-
-		# Edith wonders whether the site will remember her list. Then she sees
-		# that the site has generated a unique URL for her -- there is some
-		# explanatory text to that effect.
-
-		# She visits that URL - her to-do list is still there.
-
-# Satisfied, she goes back to sleep
-
